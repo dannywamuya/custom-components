ef083bf1538773586f6605023358d9f247f5ae42
Danny Wamuya <dwamuya@gmail.com>
2023-01-19 10:19:26 +0300

    feat: added react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,20 @@ import ReactDOM from "react-dom/client";
 import { ReactLocation, Router, Outlet } from "@tanstack/react-location";
 import routes from "./utils/routes";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 const locationClient = new ReactLocation();
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <Router location={locationClient} routes={routes}>
-        <Outlet />
-      </Router>
+      <QueryClientProvider client={queryClient}>
+        <Router location={locationClient} routes={routes}>
+          <Outlet />
+        </Router>
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
