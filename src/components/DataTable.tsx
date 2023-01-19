efd6d12efd138858ccf43ca8247c005fba925a77
Danny Wamuya <dwamuya@gmail.com>
2023-01-19 16:52:10 +0300

    feat: added columns toggle

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -3,10 +3,21 @@ import {
   getCoreRowModel,
   flexRender,
 } from "@tanstack/react-table";
-import { Flex, Text, Spinner } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  Spinner,
+  Checkbox,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import { capitalizeFirstLetter } from "../utils/textFormatter";
 import "../css/DataTable.css";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 interface DataTableProps<T> {
   dataKey: Array<string>;
@@ -34,12 +45,38 @@ function DataTable<T>({
 
   return (
     <div style={{ width: "100%", padding: "1rem" }}>
+      {/* Table Title and Description  */}
       <Flex p={"4"} w="full" gap="2" align={"center"}>
         <Text fontSize={"xl"} fontWeight="bold">
           {tableTitle ?? capitalizeFirstLetter(dataKey)}
         </Text>
         {isFetching ? <Spinner size={"sm"} /> : null}
       </Flex>
+
+      {/* Table Controls */}
+      <Flex my={"4"} w="full" gap="2" align={"center"}>
+        <Menu closeOnSelect={false}>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            Toggle Columns
+          </MenuButton>
+          <MenuList>
+            {table.getAllLeafColumns().map((column) => (
+              <MenuItem key={column.id}>
+                <Checkbox
+                  size={"md"}
+                  checked={column.getIsVisible()}
+                  onChange={column.getToggleVisibilityHandler()}
+                  defaultChecked
+                >
+                  {column.id}
+                </Checkbox>
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+      </Flex>
+
+      {/* Actual Table */}
       <table
         {...{
           style: {
